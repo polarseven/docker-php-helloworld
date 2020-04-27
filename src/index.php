d9b46aa4b127c15004b52906473a805f43019518
Adam Cousins <acousins23@gmail.com>
2020-04-27 15:40:37 +1000

    update code

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
+echo "Hello, World from Docker Deployed By PolarSeven! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
