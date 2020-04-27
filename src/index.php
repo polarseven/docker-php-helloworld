cbe35f78ed1adbcebb33f75a60500ee4781d96ce
Adam Cousins <acousins23@gmail.com>
2020-04-27 21:54:01 +1000

    remove broken file

--- src/index.php
+++ src/index.php
@@ -3,6 +3,5 @@
 echo "Hello, World from Docker Deployed By PolarSeven 2! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<img src="/mnt/efs/localfile.jpg"/>';
 
 ?>
