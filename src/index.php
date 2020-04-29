69770b5414d98ccdff8ff7ef86204d516138dc25
Adam Cousins <acousins23@gmail.com>
2020-04-29 20:32:09 +1000

    dump all env vars

--- src/index.php
+++ src/index.php
@@ -3,6 +3,6 @@
 echo "Hello, World from Docker Deployed By PolarSeven 2! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-var_dump(getenv('DB_NAME'));
+var_dump(getenv());
 
 ?>
